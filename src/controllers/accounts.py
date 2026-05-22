734941e6e3dadb14b4d4306090f1d6d3d5f69911
Guilherme Pereira <guisil_ps@hotmail.com>
2026-05-21 21:02:31 -0300

    Scaffolding bank api project

--- src/controllers/accounts.py
+++ src/controllers/accounts.py
@@ -1,11 +1,23 @@
 from fastapi import APIRouter
+from ..schemas.accounts import AccountsSchema
+from datetime import datetime
 
 router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 @router.post("/create")
-def create_account():
+def create_account(fullname: str, initial_deposit: float = 0.0):
+    #TODO Add logic to create an account into database. It can be a public endpoint
+    # if fullname is None or fullname.strip() == "":
+        
+
     return {"message": "Account created successfully"}
 
-@router.get("/{account_id}")
+@router.get("/{account_id}", response_model=AccountsSchema)
 def get_account(account_id: int):
-    return {"account_id": account_id}
+    return {
+        "id": account_id,
+        "full_name": "John Doe",
+        "account_number": f"ACC-{account_id}",
+        "balance": 1000.0,
+        "created_at": datetime.now().isoformat()
+    }
